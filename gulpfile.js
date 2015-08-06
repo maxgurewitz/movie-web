6a289fef8a05759b1566e200a78f2b0452c353e5
maxgurewitz <maxthegeek1@gmail.com>
2015-08-06 02:08:49 -0700

    react with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var reactify = require('reactify');
 
 gulp.task('browserify', function() {
-  return browserify(__dirname + '/src/client/app.js')
+  return browserify({
+       entries: __dirname + '/src/client/app.js',
+       transform: [reactify]
+     })
     .bundle()
     .pipe(source('appBundle.js'))
     .pipe(gulp.dest(__dirname + '/public/js/'));
@@ -14,11 +18,11 @@ gulp.task('watch', function() {
   gulp.watch(__dirname + '/src/client/**/*', ['browserify']);
 });
 
-gulp.task('start', ['watch'], function() {
+gulp.task('start', ['browserify', 'watch'], function() {
   nodemon({
     watch: __dirname + '/src/app/',
     script: __dirname + '/scripts/server.js',
-    ext: 'js ejs jsx',
+    ext: 'js ejs jsx json',
     watch: __dirname + '/src/app/',
     env: { 'NODE_ENV': 'development' }
   });
---
+++ src/client/app.js
@@ -0,0 +1,6 @@
+var React = require('react');
+
+React.render(
+  <h1>Dude</h1>,
+  document.getElementById('app')
+);
