d7ddd52468cdf79cdfc450cf8290e3ec707f8124
maxgurewitz <maxthegeek1@gmail.com>
2015-08-06 00:42:07 -0700

    first commit

--- scripts/server.js
+++ scripts/server.js
@@ -8,11 +8,15 @@ var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 3000 });
 
 server.register(registered, function(err) {
-  if (err) { return console.error('Failed to load a plugin:', err); }
+  if (err) {
+    return console.error('Failed to load a plugin:', err);
+  }
 });
 
 server.start(function(err) {
-  if (err) { return console.error("Failed to start server: " +  err); }
+  if (err) {
+    return console.error("Failed to start server: " +  err);
+  }
 
   console.log("Server Started.");
 });
--- src/app/registered.js
+++ src/app/registered.js
@@ -1,21 +1,10 @@
-
 GLOBAL.__BASE = __dirname;
 
+var plugins = require('./plugins');
+
 module.exports = [
-  // {
-  //   register: plugins.auth.index,
-  //   options: {}
-  // },
-  // {
-  //   register: plugins.static.index,
-  //   options: {}
-  // },
-  // {
-  //   register: plugins.views.index,
-  //   options: {}
-  // },
-  // {
-  //   register: plugins.users.index,
-  //   options: {}
-  // },
+  {
+    register: plugins.views,
+    options: {}
+  },
 ];
