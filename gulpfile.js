b76a80eac8acc498daef9c12c3cbe357f179487a
maxgurewitz <maxthegeek1@gmail.com>
2015-08-06 01:42:29 -0700

    browserify gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
 
-gulp.task('start', function () {
+gulp.task('browserify', function() {
+  return browserify(__dirname + '/src/client/app.js')
+    .bundle()
+    .pipe(source('appBundle.js'))
+    .pipe(gulp.dest(__dirname + '/public/js/'));
+});
+
+gulp.task('start', function() {
   nodemon({
     watch: __dirname + '/src/app/',
     script: __dirname + '/scripts/server.js',
