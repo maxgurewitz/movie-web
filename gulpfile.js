d6daddcc4ce1d69117f7f5dade6a277453d949fb
maxgurewitz <maxthegeek1@gmail.com>
2015-08-06 02:31:01 -0700

    sass task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var nodemon = require('gulp-nodemon');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var reactify = require('reactify');
+var sass = require('gulp-sass');
 
 gulp.task('browserify', function() {
   return browserify({
@@ -14,11 +15,18 @@ gulp.task('browserify', function() {
     .pipe(gulp.dest(__dirname + '/public/js/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(__dirname + '/src/client/**/*', ['browserify']);
+gulp.task('scss', function() {
+  gulp.src(__dirname + '/src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(__dirname + '/public/css'));
 });
 
-gulp.task('start', ['browserify', 'watch'], function() {
+gulp.task('watch', function() {
+  gulp.watch(__dirname + '/src/client/**/*', ['browserify']);
+  gulp.watch(__dirname + '/src/scss/**/*.scss', ['scss']);
+});
+
+gulp.task('start', ['browserify', 'scss', 'watch'], function() {
   nodemon({
     watch: __dirname + '/src/app/',
     script: __dirname + '/scripts/server.js',
