d323ecc16c9474954ceccee73e24b909029c6b08
maxgurewitz <maxthegeek1@gmail.com>
2015-09-05 17:05:26 -0700

    server error view

--- src/app/controllers/views.js
+++ src/app/controllers/views.js
@@ -9,3 +9,15 @@ module.exports.renderLogin = function(request, reply) {
     reply.view('login');
   }
 };
+
+module.exports.renderRegister = function(request, reply) {
+  if(request.auth.isAuthenticated) {
+    reply.redirect('/');
+  } else {
+    reply.view('register');
+  }
+};
+
+module.exports.renderServerError = function(request, reply) {
+  reply.view('serverError');
+}
